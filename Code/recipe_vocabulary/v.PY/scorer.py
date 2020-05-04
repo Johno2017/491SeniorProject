d1ca90f2167397a2fcce654f99e6d23206d5c1ee
Alex Tran <alextran1357@gmail.com>
2020-05-04 09:04:24 -0700

    Added user input to scorer

--- Code/recipe_vocabulary/v.PY/scorer.py
+++ Code/recipe_vocabulary/v.PY/scorer.py
@@ -1,3 +1,5 @@
+import os, re, string
+
 '''
 Take data out of the master recipe file
 '''
@@ -41,10 +43,28 @@ for i in data:
     noun_list.append(i.strip())
 file.close()
 
-# Test sentence
-test_paragraph1 = "toast split bread under broiler. Remove bread when it is toasted golden brown in color. Brush bread liberally with garlic oil. Sprinkle with cheese, if using, and parsley. If you added cheese, return to broiler and brown 30 seconds. Cut into chunks and serve."
-test_paragraph = "the punishment assigned to a defendant found guilty by a court, or fixed by law for a particular offense."
+'''
+This takes in user inputs. It takes into account of
+multiple paragrapgs which is unable to be done with input()
+'''
+print("INPUT. Press 'Enter' -> 'Ctrl+D' to finish:")
+contents = []
+test_paragraph = ""
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    contents.append(line)
+printable = set(string.printable)
+for cstring in contents:
+    cstring = ''.join(filter(lambda x: x in string.printable, cstring))
+    test_paragraph = cstring.translate(str.maketrans('', '', string.punctuation)).lower().strip()
+del contents
 
+'''
+Sentence cleaner 
+'''
 # Remove unwanted punctuation / convert to list
 remove = "`~!@#$%^&*()-_=+[]{};:,<.>/?\|"
 for char in remove:
@@ -52,13 +72,9 @@ for char in remove:
 test_paragraph = test_paragraph.lower()
 paragraph_list = list(test_paragraph.split(" ")) 
 
-for word in paragraph_list:
-    if (word in master_data_dic):
-        score = master_data_dic[word]
-    else:
-        score = ""
-    print(word , " " , score)
-
+'''
+Scorer
+'''
 # Generate an average score for the paragraph
 total_words = len(paragraph_list)
 current_score = 0
@@ -76,8 +92,11 @@ for word in paragraph_list:
   
 score = current_score / total_words
 print(score)        
-    
 
+if (score > 20):
+    print("YES")
+else:
+    print("NO")
 
 
 
